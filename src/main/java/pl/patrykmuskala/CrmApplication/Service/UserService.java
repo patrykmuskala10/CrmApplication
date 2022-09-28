66d1db8b3a7cba7f39b33c924c1ede970692afe0
patrykmuskala10 <patrykmuskala10@gmail.com>
2022-09-28 13:37:14 +0200

    Added Service package.

--- src/main/java/pl/patrykmuskala/CrmApplication/Service/UserService.java
+++ src/main/java/pl/patrykmuskala/CrmApplication/Service/UserService.java
@@ -1,4 +1,4 @@
-package pl.patrykmuskala.CrmApplication;
+package pl.patrykmuskala.CrmApplication.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
